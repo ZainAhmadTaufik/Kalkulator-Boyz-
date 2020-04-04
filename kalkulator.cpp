b6da4a4226d99e871642e080ffcc1ace660bc4ef
ZAIN AHMAD TAUFIK <62564112+ZainAhmadTaufik@users.noreply.github.com>
2020-04-04 14:43:56 +0700

    hapus karakter (-) dan menambah fungsi int main

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -47,5 +47,60 @@ int pembagian(){
 	cout<<"masukkan nilai b :";
 	cin>>m.b;
 	m.hasil=m.a/m.b;
-	cout<<m.a<<" -/"<<m.b<<" = "<<m.hasil;
+	cout<<m.a<<" / "<<m.b<<" = "<<m.hasil;
+}
+
+int main(){
+	menu :
+	int u;
+	cout<<"--Kalkulator Sederhana--\n";
+	cout<<"1.Perkalian\n";
+	cout<<"2.Pembagian\n";
+	cout<<"3.Pertambahan\n";
+	cout<<"4.Pengurangan\n";
+	cout<<"\nsilahkan pilih operator yang ingin anda jalankan (1-4) :";
+	cin>>u;
+	
+	if(u==1){
+		system("CLS");
+		cout<<"Operasi perkalian\n";
+		perkalian();
+		cout<<endl;
+	}
+	else if(u==2){
+		system("CLS");
+		cout<<"Operasi Pembagian\n";
+		pembagian();
+		cout<<endl;
+	}
+	else if(u==3){
+		system("CLS");
+		cout<<"Operasi Pertambahan\n";
+		pertambahan();
+		cout<<endl;
+	}
+	else if(u==4){
+		system("CLS");
+		cout<<"Operasi Pengurangan\n";
+		pengurangan();
+		cout<<endl;
+	}
+	else{
+		system("CLS");
+		cout<<"maaf permintaan anda, tidak tersedia\n";
+	}
+	
+	
+	char jawab;
+	cout<<"\nIngin kembali ke menu ? (y/n) =";
+	cin>>jawab;
+	if(jawab=='y'){
+		system("CLS");
+		goto menu;
+	}else{
+		goto exit;
+	}
+	exit:
+		return 0;
+
 }
